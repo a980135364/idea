9f167ca7ba731829f927edd72e55b5a19c971e64
xy <980135364@qq.com>
2018-12-30 09:23:04 +0800

    呵呵

--- src/main/java/com/itheima/controller/Demo.java
+++ src/main/java/com/itheima/controller/Demo.java
@@ -3,7 +3,6 @@ package com.itheima.controller;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("呵呵哒");
-        System.out.println("呵呵");
+        System.out.println("颠三倒四");
     }
 }
